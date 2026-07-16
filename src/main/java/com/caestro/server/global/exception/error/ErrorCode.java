c6f8bcc1d24162ae90f12526bdeaed4d21e6bde8
vkfro2293 <vkfro2293@naver.com>
2026-07-16 12:17:39 +0900

    feat: TURN 임시 자격증명 발급 API 구현

--- src/main/java/com/caestro/server/global/exception/error/ErrorCode.java
+++ src/main/java/com/caestro/server/global/exception/error/ErrorCode.java
@@ -32,6 +32,7 @@ public enum ErrorCode {
     LITE_TOKEN_INVALID(401, "유효하지 않은 라이트 모드 참여 토큰입니다"),
     LITE_CONCURRENT_CONNECTION(409, "이미 활성화된 연결이 있는 토큰입니다"),
     INVALID_SIGNALING_MESSAGE(400, "유효하지 않은 시그널링 메시지입니다"),
+    TURN_CREDENTIAL_FAILED(500, "TURN 자격증명 생성에 실패했습니다"),
     INTERNAL_SERVER_ERROR(500, "서버 오류가 발생했습니다");
 
     private final int status;
